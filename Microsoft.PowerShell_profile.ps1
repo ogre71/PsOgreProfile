337811ac077dc129529d73e8994d422d36499672
Jason Secrest <ogre71@gmail.com>
2020-11-08 16:17:46 -0500

    Work on new-qrcode

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -203,3 +203,27 @@ function Scratch() {
 
 	set-location $today
 }
+
+function New-QRCode{ 
+	Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [string]$url,
+
+		[Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$false,
+                   Position=1)]
+        [string]$filename
+    )
+	process {
+		get-location | Set-Variable oldLocation
+		push-location 
+		set-location C:\Users\ogre7\scratch\20201107\java1\thisIsTheArtifactId
+		mvn exec:java -D exec.mainClass=thisIsTheGroupId.App -D exec.arguments="$url,$oldLocation/$filename"
+		Pop-Location
+	}
+
+}
